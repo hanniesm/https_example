75302a0c5bcf58e4ef66a9d3634de79b19652d6a
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-03-10 18:46:18 +0000

    step 5 of https exercise

--- https_example.js
+++ https_example.js
@@ -3,13 +3,14 @@ var https = require('https');
 console.log('I did it!');
 
 var options = {
-  host: 'www.gmail.com',
+  host: 'www.example.org',
   path: '/'
 };
 
 // called by https when the request is made.
 var callback = function() {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
